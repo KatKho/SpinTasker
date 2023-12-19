5fa2fa813679c5a528e717e2c9b1606ecf5d6c1c
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-12-19 14:48:02 -0800

    login screen and navigation

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
+// import SignupScreen from '../screens/SignupScreen';
+// import MainDashboard from '../screens/MainDashboard';
+
+const Stack = createStackNavigator();
+
+function StackNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={LoginScreen} />
+      {/* <Stack.Screen name="Signup" component={SignupScreen} /> */}
+      {/* <Stack.Screen name="Main" component={MainDashboard} /> */}
+    </Stack.Navigator>
+  );
+}
+
+export default StackNavigator;
