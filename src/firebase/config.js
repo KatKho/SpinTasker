b5a701e763831bf4413db5b834774b86d1f293ac
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2024-03-01 12:26:18 -0800

    env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBxUeHxRqdAxElUO0uskvKQgi6ZIgO2UcU',
-  authDomain: 'todospi.firebaseapp.com',
-  databaseURL: 'https://todospi-default-rtdb.firebaseio.com',
-  projectId: 'todospi',
-  storageBucket: 'todospi.appspot.com',
-  messagingSenderId: '793504085772',
-  appId: '1:793504085772:ios:429634912b96ff5665bb29',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 window.navigator.userAgent = "ReactNative";
